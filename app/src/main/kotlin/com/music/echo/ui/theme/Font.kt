46c9c7ee96ae894797d9edf41e392070148ad7e7
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:44:15 +0530

    refactor(identity): migrate Echo package and branding references

--- app/src/main/kotlin/com/music/echo/ui/theme/Font.kt
+++ app/src/main/kotlin/com/music/echo/ui/theme/Font.kt
@@ -1,9 +1,9 @@
-package echo.music.iad1tya.ui.theme
+package com.nivukx.music.ui.theme
 
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import echo.music.iad1tya.R
+import com.nivukx.music.R
 
 val bbhBartle = FontFamily(
     Font(R.font.bbh_bartle_regular, FontWeight.Normal)
