ef84a1a2eddf358639615a94b95b98aaceeab758
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:49:14 +0530

    refactor(urls): remove legacy Echo first-party endpoints

--- app/src/main/kotlin/com/music/echo/ui/screens/settings/DiscordExperimental.kt
+++ app/src/main/kotlin/com/music/echo/ui/screens/settings/DiscordExperimental.kt
@@ -80,7 +80,7 @@ fun DiscordExperimental(navController: NavController) {
     val (button2CustomUrl, onButton2CustomUrlChange) =
         rememberPreference(
             key = DiscordActivityButton2CustomUrlKey,
-            defaultValue = "https://echomusic.fun",
+            defaultValue = "https://github.com/nivash01/Echo",
         )
 
     Scaffold { inner ->
