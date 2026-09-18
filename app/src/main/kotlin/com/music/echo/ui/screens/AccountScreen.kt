131a525753fbe8a6ebc94d068b6b27af8b5dcb46
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:53:02 +0530

    refactor(package): migrate Echo namespace references to Nivukx

--- app/src/main/kotlin/com/music/echo/ui/screens/AccountScreen.kt
+++ app/src/main/kotlin/com/music/echo/ui/screens/AccountScreen.kt
@@ -1,6 +1,6 @@
 
 
-package echo.music.iad1tya.ui.screens
+package com.nivukx.music.ui.screens
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
@@ -26,24 +26,24 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
-import echo.music.iad1tya.LocalPlayerAwareWindowInsets
-import echo.music.iad1tya.R
-import echo.music.iad1tya.constants.GridItemSize
-import echo.music.iad1tya.constants.GridItemsSizeKey
-import echo.music.iad1tya.constants.GridThumbnailHeight
-import echo.music.iad1tya.ui.component.ChipsRow
-import echo.music.iad1tya.ui.component.IconButton
-import echo.music.iad1tya.ui.component.LocalMenuState
-import echo.music.iad1tya.ui.component.YouTubeGridItem
-import echo.music.iad1tya.ui.component.shimmer.GridItemPlaceHolder
-import echo.music.iad1tya.ui.component.shimmer.ShimmerHost
-import echo.music.iad1tya.ui.menu.YouTubeAlbumMenu
-import echo.music.iad1tya.ui.menu.YouTubeArtistMenu
-import echo.music.iad1tya.ui.menu.YouTubePlaylistMenu
-import echo.music.iad1tya.ui.utils.backToMain
-import echo.music.iad1tya.utils.rememberEnumPreference
-import echo.music.iad1tya.viewmodels.AccountContentType
-import echo.music.iad1tya.viewmodels.AccountViewModel
+import com.nivukx.music.LocalPlayerAwareWindowInsets
+import com.nivukx.music.R
+import com.nivukx.music.constants.GridItemSize
+import com.nivukx.music.constants.GridItemsSizeKey
+import com.nivukx.music.constants.GridThumbnailHeight
+import com.nivukx.music.ui.component.ChipsRow
+import com.nivukx.music.ui.component.IconButton
+import com.nivukx.music.ui.component.LocalMenuState
+import com.nivukx.music.ui.component.YouTubeGridItem
+import com.nivukx.music.ui.component.shimmer.GridItemPlaceHolder
+import com.nivukx.music.ui.component.shimmer.ShimmerHost
+import com.nivukx.music.ui.menu.YouTubeAlbumMenu
+import com.nivukx.music.ui.menu.YouTubeArtistMenu
+import com.nivukx.music.ui.menu.YouTubePlaylistMenu
+import com.nivukx.music.ui.utils.backToMain
+import com.nivukx.music.utils.rememberEnumPreference
+import com.nivukx.music.viewmodels.AccountContentType
+import com.nivukx.music.viewmodels.AccountViewModel
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
