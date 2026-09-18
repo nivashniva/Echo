bfd8486aca4bf019e741d8df29c042428fa3fd42
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:48:06 +0530

    refactor(urls): remove legacy Echo first-party endpoints

--- app/src/main/kotlin/com/music/echo/echomusic/UpdateNotificationHelper.kt
+++ app/src/main/kotlin/com/music/echo/echomusic/UpdateNotificationHelper.kt
@@ -31,7 +31,7 @@ object UpdateNotificationHelper {
         }
 
         
-        val apkUrl = "https://echomusic.fun"
+        val apkUrl = "https://github.com/nivash01/Echo"
         val intent = Intent(Intent.ACTION_VIEW, apkUrl.toUri())
 
         val flags = PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
