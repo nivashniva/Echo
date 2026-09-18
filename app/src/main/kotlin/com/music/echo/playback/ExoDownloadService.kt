91c08c6dc158bdaffce16af640c578255849c250
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:56:06 +0530

    refactor(package): migrate Echo namespace references to Nivukx

--- app/src/main/kotlin/com/music/echo/playback/ExoDownloadService.kt
+++ app/src/main/kotlin/com/music/echo/playback/ExoDownloadService.kt
@@ -1,6 +1,6 @@
 
 
-package echo.music.iad1tya.playback
+package com.nivukx.music.playback
 
 import android.app.Notification
 import android.app.PendingIntent
@@ -15,7 +15,7 @@ import androidx.media3.exoplayer.offline.DownloadNotificationHelper
 import androidx.media3.exoplayer.offline.DownloadService
 import androidx.media3.exoplayer.scheduler.PlatformScheduler
 import androidx.media3.exoplayer.scheduler.Scheduler
-import echo.music.iad1tya.R
+import com.nivukx.music.R
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
