7e5741886e6f71af90234d5b3e53db76a449d2c2
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 14:38:40 +0530

    refactor(package): finish Nivukx library namespace cleanup

--- unison/src/main/kotlin/com/nivukx/music/unison/Unison.kt
+++ unison/src/main/kotlin/com/nivukx/music/unison/Unison.kt
@@ -1,6 +1,6 @@
 
 
-package com.music.echo.unison
+package com.nivukx.music.unison
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
@@ -15,9 +15,9 @@ import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.currentCoroutineContext
 import kotlinx.coroutines.ensureActive
 import kotlinx.serialization.json.Json
-import com.music.echo.unison.models.UnisonEntry
-import com.music.echo.unison.models.UnisonResponse
-import com.music.echo.unison.models.UnisonSearchResponse
+import com.nivukx.music.unison.models.UnisonEntry
+import com.nivukx.music.unison.models.UnisonResponse
+import com.nivukx.music.unison.models.UnisonSearchResponse
 
 object Unison {
     private const val API_BASE_URL = "https://unison.boidu.dev/"
