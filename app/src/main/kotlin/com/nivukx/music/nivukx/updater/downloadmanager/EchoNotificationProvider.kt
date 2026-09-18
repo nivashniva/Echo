35ced6bbd7b70b3c8331e7f5cabb061708ac3ef5
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 14:44:11 +0530

    refactor(package): finish Nivukx internal package namespace

--- app/src/main/kotlin/com/nivukx/music/nivukx/updater/downloadmanager/EchoNotificationProvider.kt
+++ app/src/main/kotlin/com/nivukx/music/nivukx/updater/downloadmanager/EchoNotificationProvider.kt
@@ -1,4 +1,4 @@
-package echo.music.iad1tya.echomusic.updater.downloadmanager
+package com.nivukx.music.echomusic.updater.downloadmanager
 
 import android.app.Notification
 import android.content.Context
