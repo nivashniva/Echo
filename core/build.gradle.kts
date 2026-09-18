4f5cf97d2b21071f08961da731cac493af761c4c
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 14:31:13 +0530

    refactor(build): align core namespace with Nivukx

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 android {
-    namespace = "com.music.echo.core"
+    namespace = "com.nivukx.music.core"
     compileSdk = 36
     defaultConfig { minSdk = 26 }
     compileOptions {
