f1c3efce6d3ef6fde3940b4d332cb3dfefd46b60
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 14:48:16 +0530

    refactor(identity): remove final legacy internal package references

--- app/src/main/kotlin/com/nivukx/music/nivukx/updater/downloadmanager/EchoNotificationProvider.kt
+++ app/src/main/kotlin/com/nivukx/music/nivukx/updater/downloadmanager/EchoNotificationProvider.kt
@@ -1,4 +1,4 @@
-package com.nivukx.music.echomusic.updater.downloadmanager
+package com.nivukx.music.nivukx.updater.downloadmanager
 
 import android.app.Notification
 import android.content.Context
@@ -13,7 +13,7 @@ import androidx.media3.session.MediaSession
 import com.google.common.collect.ImmutableList
 
 @OptIn(UnstableApi::class)
-class EchoNotificationProvider(
+class NivukxNotificationProvider(
     context: Context,
     notificationIdProvider: DefaultMediaNotificationProvider.NotificationIdProvider,
     channelId: String,
@@ -27,7 +27,7 @@ class EchoNotificationProvider(
         channelNameResourceId
     )
 
-    fun setSmallIcon(iconResId: Int): EchoNotificationProvider {
+    fun setSmallIcon(iconResId: Int): NivukxNotificationProvider {
         defaultProvider.setSmallIcon(iconResId)
         return this
     }
