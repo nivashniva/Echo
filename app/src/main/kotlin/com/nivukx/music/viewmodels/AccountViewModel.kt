21730cdb504055dff78d159f4d102fdf9d8642bb
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 14:35:37 +0530

    refactor(package): migrate remaining Nivukx app references

--- app/src/main/kotlin/com/nivukx/music/viewmodels/AccountViewModel.kt
+++ app/src/main/kotlin/com/nivukx/music/viewmodels/AccountViewModel.kt
@@ -1,6 +1,6 @@
 
 
-package echo.music.iad1tya.viewmodels
+package com.nivukx.music.viewmodels
 
 import android.content.Context
 import androidx.lifecycle.ViewModel
@@ -11,11 +11,11 @@ import com.music.innertube.models.ArtistItem
 import com.music.innertube.models.PlaylistItem
 import com.music.innertube.models.filterYoutubeShorts
 import com.music.innertube.utils.completed
-import echo.music.iad1tya.constants.HideYoutubeShortsKey
-import echo.music.iad1tya.ui.utils.resize
-import echo.music.iad1tya.utils.dataStore
-import echo.music.iad1tya.utils.get
-import echo.music.iad1tya.utils.reportException
+import com.nivukx.music.constants.HideYoutubeShortsKey
+import com.nivukx.music.ui.utils.resize
+import com.nivukx.music.utils.dataStore
+import com.nivukx.music.utils.get
+import com.nivukx.music.utils.reportException
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
