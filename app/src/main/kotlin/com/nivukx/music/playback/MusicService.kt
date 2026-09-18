ee8e387320462b740b649db8feee22667ca6cb91
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 22:42:23 +0530

    Use Nivukx branding for playback notifications

--- app/src/main/kotlin/com/nivukx/music/playback/MusicService.kt
+++ app/src/main/kotlin/com/nivukx/music/playback/MusicService.kt
@@ -612,7 +612,7 @@ class MusicService :
             val notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle(getString(R.string.music_player))
                 .setContentText("")
-                .setSmallIcon(R.drawable.ic_launcher_nobg)  
+                .setSmallIcon(R.drawable.app_logo_notification)  
                 .setContentIntent(pending)
                 .setOngoing(true)
                 .build()
@@ -630,7 +630,7 @@ class MusicService :
                 R.string.music_player
             )
                 .apply {
-                    setSmallIcon(R.drawable.ic_launcher_nobg)
+                    setSmallIcon(R.drawable.app_logo_notification)
                 },
         )
         player = createExoPlayer()
