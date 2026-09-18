c67cd5014c735f8b08e01a98192d9a0cf4b53528
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:51:55 +0530

    refactor(package): migrate Echo namespace references to Nivukx

--- app/src/main/kotlin/com/music/echo/extensions/ModifierExt.kt
+++ app/src/main/kotlin/com/music/echo/extensions/ModifierExt.kt
@@ -1,6 +1,6 @@
 
 
-package echo.music.iad1tya.extensions
+package com.nivukx.music.extensions
 
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.ui.Modifier
