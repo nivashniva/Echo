83b9cf2a90dff2b21cc4bc0028049370df539dad
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:52:32 +0530

    refactor(package): migrate Echo namespace references to Nivukx

--- app/src/main/kotlin/com/music/echo/ui/component/OnlineBlur.kt
+++ app/src/main/kotlin/com/music/echo/ui/component/OnlineBlur.kt
@@ -1,6 +1,6 @@
 
 
-package echo.music.iad1tya.ui.component
+package com.nivukx.music.ui.component
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
@@ -14,7 +14,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
-import echo.music.iad1tya.ui.utils.fadingEdge
+import com.nivukx.music.ui.utils.fadingEdge
 
 @Composable
 fun OnlineBlur(
