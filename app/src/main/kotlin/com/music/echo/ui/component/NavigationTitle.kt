f1a4efbf2a6f59532ba62b579f6ecb4b8863b5c1
Nivash P <137782923+nivash01@users.noreply.github.com>
2026-09-18 13:56:38 +0530

    refactor(package): migrate Echo namespace references to Nivukx

--- app/src/main/kotlin/com/music/echo/ui/component/NavigationTitle.kt
+++ app/src/main/kotlin/com/music/echo/ui/component/NavigationTitle.kt
@@ -1,6 +1,6 @@
 
 
-package echo.music.iad1tya.ui.component
+package com.nivukx.music.ui.component
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
@@ -30,7 +30,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
-import echo.music.iad1tya.R
+import com.nivukx.music.R
 
 @Composable
 fun NavigationTitle(
